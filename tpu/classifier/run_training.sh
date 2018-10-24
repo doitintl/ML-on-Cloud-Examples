2ff1e2695fad02d3950057b50c1821e7b7654ba7
gadbenram <gad@doit-intl.com>
2018-10-24 18:17:18 +0300

    Add class

--- tpu/classifier/run_training.sh
+++ tpu/classifier/run_training.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 pip install sklearn
-export  DATA_DIR=/home/gad/data/raw/
+export  DATA_DIR={PATH_DIR_HERE}
 mkdir -p $DATA_DIR
 gsutil -m cp gs://draw-me-a-sheep/images/* ./data/raw/
 git clone  -b tpu https://github.com/doitintl/ML-on-Cloud-Examples.git
 cd ML-on-Cloud-Examples/tpu/classifier/
 python train_cnn.py \
     --tpu=$TPU_NAME \
-    --use_tpu=False \
+    --use_tpu=True \
     --iterations=500 \
     --train_steps=20000\
     --data_dir=$DATA_DIR \
