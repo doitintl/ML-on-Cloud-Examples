643f0e86036ea0490a8e2be4185f2ffc23a06b46
gadbenram <gad@doit-intl.com>
2018-10-24 16:44:00 +0300

    running version

--- tpu/classifier/run_training.sh
+++ tpu/classifier/run_training.sh
@@ -3,10 +3,11 @@
 pip install sklearn
 mkdir -p ./data/raw/
 gsutil -m cp gs://draw-me-a-sheep/images/* ./data/raw/
-gsutil cp gs://tpu-demo-xsb62/code/* ./
+gid clone https://github.com/doitintl/ML-on-Cloud-Examples.git
+cd tpu/classifier/
 python train_cnn.py \
     --tpu=$TPU_NAME \
-    --use_tpu=True \
+    --use_tpu=False \
     --iterations=500 \
     --train_steps=20000\
     --data_dir=./data/raw/ \
